0d27ba739859b0f07bf43cf53dec0ba338995241
Alexey Ustinov <TotKtoSmog513@gmail.com>
2023-09-04 14:13:51 +0300

    Добавьте файлы проекта.

---
+++ SimpleMVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SimpleMVVM
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimpleMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace SimpleMVVM.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private int _CountClicks;
+        public int CountClicks
+        {
+            get 
+            { 
+                return _CountClicks; 
+            } 
+            set 
+            { 
+                _CountClicks = value; 
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            Task.Factory.StartNew(() => 
+            { 
+                while (true)
+                {
+                    Task.Delay(1000).Wait();
+                    CountClicks++;
+                }
+            });
+        }
+    }
+}
